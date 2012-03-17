00ef8c6995969032505a00d56a6b017d0d9840e5
Feth AREZKI <feth@tuttu.info>
2012-03-17 15:30:26 +0100

    license

--- fill_in.py
+++ fill_in.py
@@ -3,6 +3,12 @@
 """
 Business card generator
 Call me from command line
+
+/* This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://sam.zoy.org/wtfpl/COPYING for more details. */
 """
 
 RETURN_CODES = IO_ERR_TEMPLATE, IO_ERR_CSV, IO_ERR_COMMON_CSV, DATA_ERR = 1, 2, 3, 4
